d1d7f2ade0c699c6f84aef7db13f7842e138a704
Ihar Yachnik <jutigor@yandex.ru>
2015-06-26 01:31:31 +0300

    Add DownloadZipAsync method

--- RepositoriesDownloader/Loader.cs
+++ RepositoriesDownloader/Loader.cs
@@ -1,7 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using System.Net;
 using System.Threading.Tasks;
 
 namespace RepositoriesDownloader
@@ -11,5 +9,35 @@ namespace RepositoriesDownloader
         private const string downloadZipLink = "https://bitbucket.org/{0}/{1}/get/{2}.zip"; // https://bitbucket.org/{username}/{repository}/get/default.zip default/master
         private const string gitRepo = "master";
         private const string hgRepo = "default";
+
+        private const string appDirectory = AppDomain.CurrentDomain.BaseDirectory;
+        private const string archiveName = @"{0}\repositories\{1}.zip"; // TODO: Check
+
+        public async Task<string> DownloadZipAsync(string nickName, string repository)
+        {
+            var link = string.Format(downloadZipLink, nickName, repository, gitRepo);
+            var attemptCounter = 0;
+            string.Format(archiveName, nickName, repository);
+
+            while (true)
+            {
+                try
+                {
+                    using (var client = new WebClient())
+                    {
+                        await client.DownloadFileTaskAsync(link, archiveName);
+                        return archiveName;
+                    }
+                }
+                catch (WebException ex)
+                {
+                    if (ex.Status == WebExceptionStatus.ProtocolError)
+                        link = string.Format(downloadZipLink, nickName, repository, hgRepo);
+                    if (++attemptCounter == 3)
+                        throw new WebException("An error occurred during the download: " + archiveName);
+                    continue;
+                }
+            }
+        }
     }
 }
